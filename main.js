b7d73cba4e00444bef9293571b79d84f0696a00c
Josen336 <joseluischocobar446@gmail.com>
2022-08-20 05:58:29 -0300

    index.html and js. and css

---
+++ main.js
@@ -0,0 +1,10 @@
+// INACTIVE DESKTOP MENU
+const DesktopMenu = document.querySelector('.desktop-menu');
+
+document.querySelector('.navbar-email').addEventListener("click", function() {
+    DesktopMenu.classList.toggle('inactive');
+    console.log({DesktopMenu});
+    
+})
+// /INACTIVE DESKTOP MENU
+
